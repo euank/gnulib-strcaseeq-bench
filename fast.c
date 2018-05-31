bd013b4e4ba61eb7e3861e1f045f552db44ec26e
Euan Kemp <euank@euank.com>
2018-05-31 16:01:10 -0700

    Add 'ALREADY_UPPER' option and default to lower

--- fast.c
+++ fast.c
@@ -6,7 +6,11 @@
 volatile int x;
 
 int main() {
+#ifdef ALREADY_UPPER
+    volatile char *encoding = "UTF-8";
+#else
     volatile char *encoding = "utf-8";
+#endif
     for (int i = 0; i < 1e9; i++) {
         if (STRCASEEQ (encoding, "UTF-8", 'U', 'T', 'F', '-', '8', 0, 0, 0, 0)) {
             x = 1;
--- slow.c
+++ slow.c
@@ -5,7 +5,11 @@
 volatile int x;
 
 int main() {
+#ifdef ALREADY_UPPER
+    volatile char *encoding = "UTF-8";
+#else
     volatile char *encoding = "utf-8";
+#endif
     for (int i = 0; i < 1e9; i++) {
         if (c_strcasecmp(encoding, "UTF-8") == 0) {
             x = 1;
