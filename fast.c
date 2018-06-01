472a589af63aa87ff29f8c6441c42988bf5167e6
Euan Kemp <euank@euank.com>
2018-05-31 17:04:41 -0700

    More volatile
    A sufficiently smart compiler (aka gcc) optimizes 'fast' based on a
    const 'UTF-8' value without this.

--- fast.c
+++ fast.c
@@ -7,9 +7,9 @@ volatile int x;
 
 int main() {
 #ifdef ALREADY_UPPER
-    volatile char *encoding = "UTF-8";
+    volatile char *volatile encoding = "UTF-8";
 #else
-    volatile char *encoding = "utf-8";
+    volatile char *volatile encoding = "utf-8";
 #endif
     for (int i = 0; i < 1e9; i++) {
         if (STRCASEEQ (encoding, "UTF-8", 'U', 'T', 'F', '-', '8', 0, 0, 0, 0)) {
--- slow.c
+++ slow.c
@@ -6,9 +6,9 @@ volatile int x;
 
 int main() {
 #ifdef ALREADY_UPPER
-    volatile char *encoding = "UTF-8";
+    volatile char *volatile encoding = "UTF-8";
 #else
-    volatile char *encoding = "utf-8";
+    volatile char *volatile encoding = "utf-8";
 #endif
     for (int i = 0; i < 1e9; i++) {
         if (c_strcasecmp(encoding, "UTF-8") == 0) {
